e543b592e0b5b6514cbdbb49a939c11a98ab282c
ehdgnl5249 <ehdgnl5249@naver.com>
2021-02-13 23:23:47 +0900

    binary_search 떡볶이 떡 만들기

--- binary_search.py
+++ binary_search.py
@@ -1,20 +1,47 @@
+# def binary_search(data, target, start, end):
+#   while start <= end:
+#     mid = (start + end) // 2
+
+#     if data[mid] == target:
+#       return True
+#     elif data[mid] < target:
+#       start = mid + 1
+#     else:
+#       end = mid - 1
+#   return False
+
+# data = [8, 3, 7, 9, 2]
+# targets = [5, 7, 9]
+
+# data.sort()
+
+# for target in targets:
+#   if binary_search(data, target, 0, len(data)-1):
+#     print('yes')
+
+
+
+# 떡이 [19, 10, 15, 17] cm씩 있을 때 손님에게 6cm를 주려면 몇cm의 절단기로 잘라야 하나? 
 def binary_search(data, target, start, end):
   while start <= end:
     mid = (start + end) // 2
+    arr_sum = sum([x-mid if x-mid > 0 else 0 for x in data])
+    print(mid, arr_sum)
+    if target == arr_sum:
+      return mid
 
-    if data[mid] == target:
-      return True
-    elif data[mid] < target:
+    elif target < arr_sum:
       start = mid + 1
+      
     else:
       end = mid - 1
-  return False
+  
+  return 0
+    
 
-data = [8, 3, 7, 9, 2]
-targets = [5, 7, 9]
-
+n = 6
+data = [19, 15, 10, 17]
 data.sort()
 
-for target in targets:
-  if binary_search(data, target, 0, len(data)-1):
-    print('yes')
+
+print(binary_search(data, n, 0, data[3]))
